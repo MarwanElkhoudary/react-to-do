e18cfc4802067e3249f83ca1bcd495e14a518ae0
MarwanElkhoudary <marwangaza@hotmail.com>
2020-01-30 19:54:36 +0200

    Updates

---
+++ precache-manifest.de2541fc0028b9a09845eb097158ebc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fecd83dc761c7bc4627ddacc72e8af5",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "6e5e92664f2be53676ed",
+    "url": "/react-to-do/static/css/main.4dfff27f.chunk.css"
+  },
+  {
+    "revision": "356477a975ae764c52af",
+    "url": "/react-to-do/static/js/2.c1da4deb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-to-do/static/js/2.c1da4deb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e5e92664f2be53676ed",
+    "url": "/react-to-do/static/js/main.22eb0eb9.chunk.js"
+  },
+  {
+    "revision": "eb299a1fe02c0fe810d7",
+    "url": "/react-to-do/static/js/runtime-main.d07ac5ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
